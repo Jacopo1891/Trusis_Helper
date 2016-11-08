b2f12319f395cdde23bbcff28ea23371a7299408
Jacopo <jacopo1891@gmail.com>
2016-11-08 22:47:28 +0100

    v 0.15 Aggiunta possibilità di note personalizzate

--- TRUSIS_HELPER.user.js
+++ TRUSIS_HELPER.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @id             trusis-helper
 // @name           TRUSIS HELPER: Mentire è solo l'inizio (www.trusis.it)
-// @version        0.141
+// @version        0.15
 // @namespace
 // @updateURL      https://github.com/Jacopo1891/Trusis_Helper/raw/master/TRUSIS_HELPER.user.js
 // @downloadURL    https://github.com/Jacopo1891/Trusis_Helper/raw/master/TRUSIS_HELPER.user.js
@@ -14,39 +14,8 @@
 // ==/UserScript==
 
 $(window).on("load", function() {
-    trusis_helper();
-});
-function trusis_helper() {
-    var url = document.URL.split("/");
-    var url_part = url[3];
-    if ( url_part != "game"){
-        return;
-    }
-    var match = url[4];
-    var temp_list = ["partita" , match];
-
-    var trusis_helper_list = restoreCookie();
-    if ( trusis_helper_list === ""){
-        trusis_helper_list = [];
-        trusis_helper_list.push(temp_list);
-    }else{
-        var trusis_helper_list_temp =  $.parseJSON( trusis_helper_list );
-        if (temp_list[1] === trusis_helper_list_temp[0][1]){
-            trusis_helper_list = trusis_helper_list_temp;
-            restoreRole(trusis_helper_list);
-        }else{
-            var cancVecchiaPartita = confirm("Trusis_Helper:\nVuoi cancellare i dati della precedente partita? \n(Consigliato se si inizia una nuova)");
-            if(cancVecchiaPartita === true){
-                trusis_helper_list = [];
-                trusis_helper_list.push(temp_list);
-                saveCookie(trusis_helper_list);
-            }
-            if(cancVecchiaPartita === false){
-                alert("I ruoli inseriti in questa partita saranno ignorati.");
-            }
-        }
-    }
-
+    var trusis_helper_list = trusis_helper_role();
+    trusis_helper_note( trusis_helper_list );
     document.body.onclick = function(e) {   //Document body clicked
         if (window.event) {
             e = event.srcElement;           //e = elemento cliccato
@@ -54,7 +23,6 @@ function trusis_helper() {
         else {
             e = e.target;                   //e = elemento cliccato
         }
-
         if (e.className && e.className.indexOf('avatar_name') != -1) {
             var nickClicked = $(e).children('a').text();
 
@@ -75,15 +43,57 @@ function trusis_helper() {
                     saveCookie(trusis_helper_list);
                 }
             }
-        }if (e.id && e.id.indexOf('trusis_helper_img') != -1) {
+        }if (e.id == 'trusis_helper_img') {
             var nickRemove = $(e).parent().parent().text();
             trusis_helper_list = removePlayer(trusis_helper_list, nickRemove);
             // Salvo sui Cookie
             saveCookie(trusis_helper_list);
             $(e).remove();
-
+        }if (e.id == "helper_note"){
+            $(".home_helper_element").toggle();
+        }
+        if (e.id == "save_note_helper"){
+            trusis_helper_list = save_note_helper( trusis_helper_list );
+            document.getElementById("save_note_helper").innerHTML="Salvato!";
+            setTimeout(function(){
+                document.getElementById("save_note_helper").innerHTML="Salva";
+            },5000);
+            // Salvo sui Cookie
+                    saveCookie(trusis_helper_list);
         }
     }
+});
+function trusis_helper_role() {
+    var url = document.URL.split("/");
+    var url_part = url[3];
+    if ( url_part != "game"){
+        return;
+    }
+    var match = url[4];
+    var temp_list = ["partita" , match];
+
+    trusis_helper_list = restoreCookie();
+    if ( trusis_helper_list === ""){
+        trusis_helper_list = [];
+        trusis_helper_list.push(temp_list);
+    }else{
+        var trusis_helper_list_temp =  $.parseJSON( trusis_helper_list );
+        if (temp_list[1] === trusis_helper_list_temp[0][1]){
+            trusis_helper_list = trusis_helper_list_temp;
+            restoreRole(trusis_helper_list);
+        }else{
+            var cancVecchiaPartita = confirm("Trusis_Helper:\nVuoi cancellare i dati della precedente partita? \n(Consigliato se si inizia una nuova)");
+            if(cancVecchiaPartita === true){
+                trusis_helper_list = [];
+                trusis_helper_list.push(temp_list);
+                saveCookie(trusis_helper_list);
+            }
+            if(cancVecchiaPartita === false){
+                alert("I ruoli inseriti in questa partita saranno ignorati.");
+            }
+        }
+    }
+    return trusis_helper_list;
 }
 
 function sceltaRuolo(){
@@ -206,4 +216,94 @@ function addRolePlayer(playerHtmlElement, ruolo){
     }else{
         console.log("Errore: impossibile aggiungere il ruolo.");
     }
+}
+
+function html_note_code( player_code ){
+    var html_code = '<table class="home_helper">'
+    + '<tbody>'
+    + '<tr>'
+    + '<td class="home_tl"></td>'
+    + '<td class="home_t"><img id="helper_note" src="css/images/game/home.png" /></td>'
+    + '<td class="home_tr"></td>'
+    + '</tr>'
+    + '<tr>'
+    + '<td class="home_l"></td>'
+    + '<th class="home_c">'
+    + '<div class="home_helper_element" style="display: none;">'
+    + player_code
+    + '<div style="display: flex; flex-grow: 1; width: 100%; list-style-type: none;">'
+    +'<button style="margin: 0 auto; border-radius:6px; width:100px; background-color: #112; color: #AEAEAE; border: 0; padding: 4px 7px; text-align:center;" id="save_note_helper">Salva</button></div>'
+    +'</div>'
+    +'</th>'
+    +'<td class="home_r"></td>'
+    +'</tr>'
+    +'<tr></tr>'
+    +'<tr>'
+    +'<td class="home_bl"></td>'
+    +'<td class="home_b" style="text-align:right">'
+    +'</td>'
+    +'<td class="home_br"></td>'
+    +'</tr>'
+    +'</tbody>'
+    +'</table>';
+
+    return html_code;
+}
+
+function trusis_helper_note( l ){
+
+    // Lista di tutti gli elementi con la classe 'avatar_name'
+    var lista = document.getElementsByClassName('avatar');
+    var stringHtml = "";
+    for (var i = 0; i < lista.length; i++) {
+        var tempCard = lista[i].getElementsByClassName('avatar_name');
+        var tempNick = $(tempCard).children('a.nowrap').text();
+        var note = "";
+            for(var j = 0; j < l.length; j++){
+                if( l[j].length >2 &&l[j][0] === tempNick){
+                    note = l[j][2];
+                    break;
+                }
+            }
+        stringHtml += '<div style="display: flex; flex-grow: 1; width: 100%; list-style-type: none;">'
+            +'<li style="flex-grow: 1; vertical-align:middle; padding-left:10px; width: 30px;">'
+            + tempNick
+            +'</li><li style="flex-grow: 3; vertical-align:middle; padding-left:10px; padding-right: 20px;">'
+            +'<textarea class="trusis_helper_note" id="'
+            + tempNick
+            + '" style="resize:none; width: 100%; border-radius: 10px; background-color: #346;text-align: center; color: #CDF;">'
+            + note
+            + '</textarea></li></div><br>';
+    }
+    var html_code = html_note_code( stringHtml );
+
+    var html_player_alive = document.getElementsByClassName('home');
+    $(html_code).insertAfter(html_player_alive);
+}
+
+function save_note_helper( l ){
+    // Salva le note e aggiorna la lista in ingresso contenenti i ruoli / note già salvate
+    var lista = document.getElementsByClassName('avatar');
+    for (var i = 0; i < lista.length; i++) {
+        var tempCard = lista[i].getElementsByClassName('avatar_name');
+        var tempNick = $(tempCard).children('a.nowrap').text();
+        var textarea_area_element = "textarea#"+tempNick;
+        var temp_note = $(textarea_area_element).val();
+        if ( typeof temp_note !== 'undefined'){
+            var check = true;
+            for(var j = 0; j < l.length; j++){
+                if(l[j][0] === tempNick){
+                    l[j].push(temp_note);
+                    check = false;
+                    break;
+                }
+            }
+            if (check){
+                // Controllo che non sia una nuova informazione
+                new_value_with_note = [tempNick, "", temp_note];
+                l.push(new_value_with_note);
+            }
+        }
+    }
+    return l;
 }
