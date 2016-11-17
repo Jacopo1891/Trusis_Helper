1ec169e19f0e2148760e9cb20e985620ce853dd6
Jacopo <jacopo1891@gmail.com>
2016-11-18 00:51:06 +0100

    Wip: colori chat personalizzati

--- TRUSIS_HELPER.user.js
+++ TRUSIS_HELPER.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @id             trusis-helper
 // @name           TRUSIS HELPER: Mentire è solo l'inizio (www.trusis.it)
-// @version        0.161
+// @version        0.161wip
 // @author         Jacopo1891
 // @namespace
 // @updateURL      https://github.com/Jacopo1891/Trusis_Helper/raw/master/TRUSIS_HELPER.user.js
@@ -13,6 +13,10 @@
 // @match          https://trusis.altervista.org/*
 // @grant          none
 // ==/UserScript==
+var spectrum_js = "http://bgrins.github.io/spectrum/spectrum.js";
+var spectrum_css = "http://bgrins.github.io/spectrum/spectrum.css";
+$('head').append('<script type="text/javascript" src="' + spectrum_js + '"></script>');
+$('head').append('<link rel="stylesheet" type="text/css" href="' + spectrum_css + '">');
 
 $(window).on("load", function() {
     var trusis_helper_list = trusis_helper_role();
@@ -85,6 +89,30 @@ $(window).on("load", function() {
                 document.getElementById("export_note_helper").innerHTML="Esporta";
             },3500);
         }
+    }
+    var lista = document.getElementsByClassName('avatar');
+    for (var i = 0; i < lista.length; i++) {
+        var tempCard = lista[i].getElementsByClassName('avatar_name');
+        var tempNick = $(tempCard).children('a.nowrap').text();
+        $('#color_'+tempNick.replace(" ", "_")).spectrum({
+            color: "#AAC",
+            showPaletteOnly: true,
+            togglePaletteOnly: true,
+            palette: [
+                ["#000","#444","#666","#999","#ccc","#eee","#f3f3f3","#fff"],
+                ["#f00","#f90","#ff0","#0f0","#0ff","#00f","#90f","#f0f"],
+                ["#f4cccc","#fce5cd","#fff2cc","#d9ead3","#d0e0e3","#cfe2f3","#d9d2e9","#ead1dc"],
+                ["#ea9999","#f9cb9c","#ffe599","#b6d7a8","#a2c4c9","#9fc5e8","#b4a7d6","#d5a6bd"],
+                ["#e06666","#f6b26b","#ffd966","#93c47d","#76a5af","#6fa8dc","#8e7cc3","#c27ba0"],
+                ["#c00","#e69138","#f1c232","#6aa84f","#45818e","#3d85c6","#674ea7","#a64d79"],
+                ["#900","#b45f06","#bf9000","#38761d","#134f5c","#0b5394","#351c75","#741b47"],
+                ["#600","#783f04","#7f6000","#274e13","#0c343d","#073763","#20124d","#4c1130"]
+            ],
+            change: function(color) {
+                var id = $(this).attr('id');
+                set_color_chat (id, color.toHexString());
+            }
+        });
     }
 });
 function trusis_helper_role() {
@@ -327,11 +355,14 @@ function trusis_helper_note( l ){
                 }
             }
 
-        stringHtml += '<div style="display: flex; flex-grow: 1; width: 100%; list-style-type: none;">'
-            +'<li style="flex-grow: 1; vertical-align:middle; padding-left:10px; padding-top:10px; width: 30px;">'
+        stringHtml += '<div style="display: flex; flex-grow: 1; width: 100%; list-style-type: none; id=color_"'
+            + tempNick.replace(" ", "_")
+            + '">'
+            + '<input type="text" id="color_'+ tempNick.replace(" ", "_") +'" />'
+            + '<li style="flex-grow: 1; vertical-align:middle; padding-left:10px; padding-top:5px; width: 30px;">'
             + '<a href="'+tempNick_link+'">'+tempNick+'</a>'
-            +'</li><li style="flex-grow: 3; vertical-align:middle; padding-left:10px; padding-right: 20px;">'
-            +'<textarea class="trusis_helper_note" id="'
+            + '</li><li style="flex-grow: 3; vertical-align:middle; padding-left:10px; padding-right: 20px; margin-top:-2px; ">'
+            + '<textarea class="trusis_helper_note" id="'
             + tempNick.replace(" ", "_")
             + '" style="resize:none; width: 100%; border-radius: 10px; background-color: #346; padding-left: 10px; color: #CDF; margin: 0px 0px -20px 0px;">'
             + note
@@ -356,7 +387,6 @@ function save_note_helper( l ){
             for(var j = 0; j < l.length; j++){
                 if(l[j][0] === tempNick.replace(" ", "_")){
                     if ( l.length <= 2 ){
-                        console.log("Meno di 2");
                         l[j].splice(2, 0, temp_note);
                     }
                     else{
@@ -388,4 +418,18 @@ function export_trusis_helper_note( l ){
         }
     }
     return result;
+}
+
+function set_color_chat (id, colore){
+
+    var player = id.replace("color_", "");
+    var nick = player.replace("_", " ");
+    var lista = document.getElementsByClassName('chat_message');
+    for (var i=0; i<lista.length; i++){
+        var tempNick = $(lista[i]).children('a').text();
+        if (tempNick == nick){
+            console.log(colore);
+            $(lista[i]).css('color', colore);
+        }
+    }
 }
